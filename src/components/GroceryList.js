5d3995c60328e97728c1fbb0cd1907cef94e1e1a
robert <robert.langereis@gmail.com>
2020-01-14 14:31:23 +0100

    semicolons added

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -2,25 +2,26 @@ import React from "react";
 import GroceryListItem from "./GroceryListItem";
 
 export default class GroceryList extends React.Component {
-  render() {
-    const { groceryItems, readonly, handleClickGroceryItem } = this.props;
-    return (
-      <React.Fragment>
-        {" "}
-        <ul>
-          {groceryItems
-            ? groceryItems.map(grocery => {
-              return(
-                <GroceryListItem
-                  key={grocery.id}
-                  grocery={grocery}
-                  readonly={readonly}
-                  clickGrocery={handleClickGroceryItem}
-                />
-              )})
-            : " "}
-        </ul>
-      </React.Fragment>
-    );
-  }
+	render() {
+		const { groceryItems, readonly, handleClickGroceryItem } = this.props;
+		return (
+			<React.Fragment>
+				{" "}
+				<ul>
+					{groceryItems
+						? groceryItems.map(grocery => {
+								return (
+									<GroceryListItem
+										key={grocery.id}
+										grocery={grocery}
+										readonly={readonly}
+										clickGrocery={handleClickGroceryItem}
+									/>
+								);
+						  })
+						: " "}
+				</ul>
+			</React.Fragment>
+		);
+	}
 }
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -2,14 +2,14 @@ import React from "react";
 import GroceryList from "./GroceryList";
 
 const ShoppingCart = ({ groceryItems, handleClickEmptyCart }) => {
-  return (
-    <React.Fragment>
-      <button className="add-grocery-button" onClick={handleClickEmptyCart}>
-        Empty Cart
-      </button>
-      <GroceryList readonly={true} groceryItems={groceryItems} />
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<button className="add-grocery-button" onClick={handleClickEmptyCart}>
+				Empty Cart
+			</button>
+			<GroceryList readonly={true} groceryItems={groceryItems} />
+		</React.Fragment>
+	);
 };
 
 export default ShoppingCart;
