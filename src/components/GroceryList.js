22a67f3d479d33dc6eaa87779af965d2a761e5ee
Niels Bom <niels@nielsbom.com>
2020-01-16 09:22:11 +0100

    Move map of items outside of render function.

--- src/components/GroceryList.js
+++ src/components/GroceryList.js
@@ -4,20 +4,20 @@ import GroceryListItem from "./GroceryListItem";
 class GroceryList extends React.Component {
   render() {
     const { groceryItems, readonly, handleClickGroceryItem } = this.props;
+    const listItems = groceryItems
+      ? groceryItems.map(grocery => (
+          <GroceryListItem
+            key={grocery.id}
+            grocery={grocery}
+            readonly={readonly}
+            clickGrocery={handleClickGroceryItem}
+          />
+        ))
+      : " ";
+
     return (
       <React.Fragment>
-        <ul>
-          {groceryItems
-            ? groceryItems.map(grocery => (
-                <GroceryListItem
-                  key={grocery.id}
-                  grocery={grocery}
-                  readonly={readonly}
-                  clickGrocery={handleClickGroceryItem}
-                />
-              ))
-            : " "}
-        </ul>
+        <ul>{listItems}</ul>
       </React.Fragment>
     );
   }
